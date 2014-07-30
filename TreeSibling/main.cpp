d5430295d03394569fe21ae507d4efa531309a2e
Xethik <mr.michaelhogan@gmail.com>
2014-07-30 15:26:31 -0400

    Due to a realization of personal confusion over what qualifies as a sibling, rewriting most of the code. Will be deleting old code once I'm more sure of this method. Tree will be stored in 2D array rather than an actual tree.

--- TreeSibling/main.cpp
+++ TreeSibling/main.cpp
@@ -3,6 +3,7 @@
 //	and all right children have values equal to their parent + their parent's right sibling.
 
 #include "Node.h"
+#include <cmath>
 
 void createDepth(Node* curNode, int curDepth, int maxDepth) {
 	if (curDepth < maxDepth) {
@@ -13,10 +14,10 @@ void createDepth(Node* curNode, int curDepth, int maxDepth) {
 }
 
 int main() {
-	int depth = 1;
-	Node* root = new Node(1, nullptr, false);
-
-	createDepth(root, 1, 3);
-
+	int depth = 2;
+	int** tree = new int*[depth];
+	for (int i = 0; i < depth; i++) {
+		tree[i] = new int[(int)pow(2, i - 1)];
+	}
 	return 0;
 }
