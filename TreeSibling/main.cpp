2d9d27cc530d559144d7c2083f585c1f6380658c
Xethik <mr.michaelhogan@gmail.com>
2014-07-30 15:48:06 -0400

    Deleted Node class, no longer used.

--- TreeSibling/main.cpp
+++ TreeSibling/main.cpp
@@ -2,17 +2,8 @@
 //Creates a tree with depth n, where all left children have values equal to their parent + their parent's left sibling
 //	and all right children have values equal to their parent + their parent's right sibling.
 
-#include "Node.h"
 #include <cmath>
 #include <iostream>
-
-void createDepth(Node* curNode, int curDepth, int maxDepth) {
-	if (curDepth < maxDepth) {
-		curNode->createChildren();
-		createDepth(curNode->getLeftChild(), curDepth + 1, maxDepth);
-		createDepth(curNode->getRightChild(), curDepth + 1, maxDepth);
-	}
-}
 
 int main() {
 	int depth = 4;
