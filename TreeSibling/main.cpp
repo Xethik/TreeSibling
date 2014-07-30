b9a7f179d0198d6d12cac582269518de2a8c6567
Xethik <mr.michaelhogan@gmail.com>
2014-07-30 15:46:28 -0400

    New algorithm seems to be a success, current prints very ugly tree.

--- TreeSibling/main.cpp
+++ TreeSibling/main.cpp
@@ -4,6 +4,7 @@
 
 #include "Node.h"
 #include <cmath>
+#include <iostream>
 
 void createDepth(Node* curNode, int curDepth, int maxDepth) {
 	if (curDepth < maxDepth) {
@@ -14,10 +15,38 @@ void createDepth(Node* curNode, int curDepth, int maxDepth) {
 }
 
 int main() {
-	int depth = 2;
+	int depth = 4;
 	int** tree = new int*[depth];
+
+	tree[0] = new int[1];
+	tree[0][0] = 1;
+
+	for (int i = 1; i < depth; i++) {
+		int rowSize = (int)pow(2, i);
+		tree[i] = new int[rowSize];
+
+		for (int j = 0; j < rowSize; j++) {
+			int siblingVal = 0;
+			if (j % 2 == 0) { //If even, check left sibling. If odd, check right
+				if (j > 1) { //Don't check left if on edge of tree
+					siblingVal = tree[i - 1][j / 2 - 1];
+				}
+			}
+			else {
+				if (j < rowSize - 1) { //Don't check right if on edge of tree
+					siblingVal = tree[i - 1][j / 2 + 1];
+				}
+			}
+			tree[i][j] = siblingVal + tree[i-1][j/2]; //Sibling value + parent value
+		}
+	}
+
 	for (int i = 0; i < depth; i++) {
-		tree[i] = new int[(int)pow(2, i - 1)];
+		int rowSize = (int)pow(2, i);
+		for (int j = 0; j < rowSize; j++) {
+			std::cout << tree[i][j] << " ";
+		}
+		std::cout << std::endl;
 	}
 	return 0;
 }
