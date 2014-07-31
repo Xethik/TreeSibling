4d5ea18d9f14f3115ecd5bda511878b69d45a606
Xethik <mr.michaelhogan@gmail.com>
2014-07-30 22:55:36 -0400

    Added printing of the tree. Based on code from http://leetcode.com/2010/09/how-to-pretty-print-binary-tree.html

--- TreeSibling/main.cpp
+++ TreeSibling/main.cpp
@@ -4,14 +4,91 @@
 
 #include <cmath>
 #include <iostream>
+#include <iomanip>
+#include <deque>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+// Convert an integer value to string
+string intToString(int val) {
+	ostringstream ss;
+	ss << val;
+	return ss.str();
+}
+
+// Print the arm branches (eg, /    \ ) on a line
+void printBranches(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLevel, const deque<int>& nodesQueue, ostream& out) {
+	deque<int>::const_iterator iter = nodesQueue.begin();
+	for (int i = 0; i < nodesInThisLevel / 2; i++) {
+		out << ((i == 0) ? setw(startLen - 1) : setw(nodeSpaceLen - 2)) << "" << "/";
+		out << setw(2 * branchLen + 2) << "" << "\\";
+	}
+	out << endl;
+}
+
+// Print the branches and node (eg, ___10___ )
+void printNodes(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLevel, const deque<int>& nodesQueue, ostream& out) {
+	deque<int>::const_iterator iter = nodesQueue.begin();
+	for (int i = 0; i < nodesInThisLevel; i++, iter++) {
+		out << ((i == 0) ? setw(startLen) : setw(nodeSpaceLen)) << "" << ((*iter) ? setfill('_') : setfill(' '));
+		out << setw(branchLen + 2) << ((*iter) ? intToString(*iter) : "");
+		out << ((*iter) ? setfill('_') : setfill(' ')) << setw(branchLen) << "" << setfill(' ');
+	}
+	out << endl;
+}
+
+// Print the leaves only (just for the bottom row)
+void printLeaves(int indentSpace, int level, int nodesInThisLevel, const deque<int>& nodesQueue, ostream& out) {
+	deque<int>::const_iterator iter = nodesQueue.begin();
+	for (int i = 0; i < nodesInThisLevel; i++, iter++) {
+		out << ((i == 0) ? setw(indentSpace + 2) : setw(2 * level + 2)) << ((*iter) ? intToString(*iter) : "");
+	}
+	out << endl;
+}
+
+// Pretty formatting of a binary tree to the output stream
+// @ param
+// level  Control how wide you want the tree to sparse (eg, level 1 has the minimum space between nodes, while level 2 has a larger space between nodes)
+// indentSpace  Change this to add some indent space to the left (eg, indentSpace of 0 means the lowest level of the left node will stick to the left margin)
+void printPretty(int** tree, int level, int indentSpace, int depth, ostream& out) {
+	int h = depth;
+	int nodesInThisLevel = 1;
+
+	int branchLen = 2 * ((int)pow(2.0, h) - 1) - (3 - level)*(int)pow(2.0, h - 1);  // eq of the length of branch for each node of each level
+	int nodeSpaceLen = 2 + (level + 1)*(int)pow(2.0, h);  // distance between left neighbor node's right arm and right neighbor node's left arm
+	int startLen = branchLen + (3 - level) + indentSpace;  // starting space to the first node to print of each level (for the left most node of each level only)
+
+	deque<int> nodesQueue;
+	nodesQueue.push_back(tree[0][0]);
+	for (int r = 0; r < h - 1; r++) {
+		printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+		branchLen = branchLen / 2 - 1;
+		nodeSpaceLen = nodeSpaceLen / 2 + 1;
+		startLen = branchLen + (3 - level) + indentSpace;
+		printNodes(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+		for (int i = 0; i < nodesInThisLevel; i++) {
+			nodesQueue.pop_front();
+
+			nodesQueue.push_back(tree[r + 1][i * 2]);
+			nodesQueue.push_back(tree[r + 1][i * 2 + 1]);
+
+		}
+		nodesInThisLevel *= 2;
+	}
+	printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+	printLeaves(indentSpace, level, nodesInThisLevel, nodesQueue, out);
+}
 
 int main() {
 	int depth;
-	std::cout << "Enter a tree depth ( > 0): ";
-	while (!(std::cin >> depth)) {
-		std::cout << "Invalid input, enter an integer greater than zero." << std::endl << "Enter a tree depth ( > 0): ";
-		std::cin.clear();
-		std::cin.ignore('\n');
+	cout << "Enter a tree depth ( > 0): ";
+	while (!(cin >> depth)) {
+		cout << "Invalid input, enter an integer greater than zero." << endl << "Enter a tree depth ( > 0): ";
+		cin.clear();
+		cin.ignore('\n');
 	}
 	int** tree = new int*[depth];
 
@@ -38,12 +115,6 @@ int main() {
 		}
 	}
 
-	for (int i = 0; i < depth; i++) {
-		int rowSize = (int)pow(2, i);
-		for (int j = 0; j < rowSize; j++) {
-			std::cout << tree[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+	printPretty(tree, 1, 0, depth, cout);
 	return 0;
 }
